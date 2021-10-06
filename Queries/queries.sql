b6495349e1ddcb568d70a7ce4dcac2c7bc810b60
Siyada <Siyada_World@outlook.com>
2021-10-06 16:38:11 -0400

    Department Info

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -122,7 +122,7 @@ SELECT 	ce.emp_no,
 		d.dept_name		
 INTO dept_info
 FROM current_emp as ce
-INNER JOIN dept_emp AS de
-ON (ce.emp_no = de.emp_no)
-INNER JOIN departments AS d
-ON (de.dept_no = d.dept_no);
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no);
